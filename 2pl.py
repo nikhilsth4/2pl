92b48bbc6aca5b8235d41aab19241eb765e18a33
nikhil <nikhilshrestha0@gmail.com>
2023-10-28 00:04:01 -0500

    read operation complete

--- 2pl.py
+++ 2pl.py
@@ -1,15 +1,18 @@
+import re
 
 
 def begin_transaction(transaction_id, transaction_table, global_timestamp):
-
     if transaction_id not in transaction_table:
         transaction = {
             "transaction_state": "active",
             "locked_items": [],
-            "timestamp": global_timestamp
+            "timestamp": global_timestamp,
         }
         transaction_table[transaction_id] = transaction
-        print(f"Transaction T{transaction_id} is created and starts at TS({global_timestamp}).")
+        print(
+            f"Transaction T{transaction_id} is created and starts at TS({global_timestamp})."
+        )
+
 
 def read_item(transaction_id, item_id, transaction_table, lock_table, global_timestamp):
     current_transaction = transaction_table[transaction_id]
@@ -19,42 +22,53 @@ def read_item(transaction_id, item_id, transaction_table, lock_table, global_tim
             if current_lock["holding_transaction"] == transaction_id:
                 print(f"T{transaction_id} reads {item_id}.")
             else:
-                if current_transaction["timestamp"] < current_lock["holding_transaction"]["timestamp"]:
+                if (
+                    current_transaction["timestamp"]
+                    < current_lock["holding_transaction"]["timestamp"]
+                ):
                     current_transaction["transaction_state"] = "waiting"
                     current_lock["waiting_transactions"].append(transaction_id)
-                    print(f"T{transaction_id} is younger and waits for {current_lock['holding_transaction']} to release {item_id}.")
+                    print(
+                        f"T{transaction_id} is younger and waits for {current_lock['holding_transaction']} to release {item_id}."
+                    )
                 else:
                     current_transaction["transaction_state"] = "aborted"
-                    print(f"T{transaction_id} is older and aborted due to conflict with T{current_lock['holding_transaction']} on {item_id}.")
+                    print(
+                        f"T{transaction_id} is older and aborted due to conflict with T{current_lock['holding_transaction']} on {item_id}."
+                    )
         else:
-            if current_transaction["timestamp"] < current_lock["holding_transaction"]["timestamp"]:
+            if (
+                current_transaction["timestamp"]
+                < current_lock["holding_transaction"]["timestamp"]
+            ):
                 current_transaction["transaction_state"] = "waiting"
                 current_lock["waiting_transactions"].append(transaction_id)
-                print(f"T{transaction_id} is younger and waits for {current_lock['holding_transaction']} to release {item_id}.")
+                print(
+                    f"T{transaction_id} is younger and waits for {current_lock['holding_transaction']} to release {item_id}."
+                )
             else:
                 current_transaction["transaction_state"] = "aborted"
-                print(f"T{transaction_id} is older and aborted due to conflict with T{current_lock['holding_transaction']} on {item_id}.")
+                print(
+                    f"T{transaction_id} is older and aborted due to conflict with T{current_lock['holding_transaction']} on {item_id}."
+                )
     else:
         lock_table[item_id] = {
             "lock_state": "read",
             "holding_transaction": transaction_id,
-            "waiting_transactions": []
+            "waiting_transactions": [],
         }
         current_transaction["locked_items"].append(item_id)
         print(f"T{transaction_id} reads {item_id} (read-locks it).")
 
 
 def parse_operation(line):
-    line = line.rstrip(';')
-    parts = line.split()
-    op = parts[0]
-    if len(parts) > 1:
-        tid = int(parts[0][1:])  # Extract the numeric part from the transaction ID
-        if len(parts) > 2:
-            item = parts[2].strip('()')
-            return op, tid, item
-        return op, tid
-    return op
+    match = re.match(r"([brwe])(\d+)\s*(\((\w+)\))?;", line)
+    if match:
+        op = match.group(1)
+        tid = int(match.group(2))
+        item = match.group(4)
+        return op, tid, item
+    return None
 
 
 def simulate_schedule(schedule_file):
@@ -66,12 +80,12 @@ def simulate_schedule(schedule_file):
         for line in file:
             line = line.strip()
             op, tid, *rest = parse_operation(line)
-            print(op,tid,rest)
-            
-            if op == 'b':
+            print(op, tid, rest)
+
+            if op == "b":
                 begin_transaction(tid, transaction_table, global_timestamp)
                 global_timestamp += 1
-            elif op == 'r':
+            elif op == "r":
                 item = rest[0]
                 print(tid, item, transaction_table, lock_table, global_timestamp)
                 read_item(tid, item, transaction_table, lock_table, global_timestamp)
